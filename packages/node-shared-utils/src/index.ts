70b6e2df5187a9d0b363ae30ca5056c7c22e5b95
cszatma <christopherszatmary@gmail.com>
2018-05-13 16:30:00 -0400

    Installed ora, added spinner util to node-shared-utils

--- packages/node-shared-utils/src/index.ts
+++ packages/node-shared-utils/src/index.ts
@@ -1,2 +1,5 @@
 export * from './commandExists';
 export * from './logger';
+export * from './spinner';
+
+export type Optional<T> = T | undefined;
---
+++ packages/node-shared-utils/src/spinner.ts
@@ -0,0 +1,55 @@
+import chalk from 'chalk';
+import ora from 'ora';
+
+import { Optional } from '.';
+
+export interface Message {
+  symbol: string;
+  text: string;
+}
+
+const spinner = ora();
+let lastMsg: Optional<Message>;
+
+export function logWithSpinner(symbol: string, message?: string): void {
+  if (!message) {
+    message = symbol;
+    symbol = chalk.green('✔');
+  }
+
+  if (lastMsg) {
+    spinner.stopAndPersist({
+      symbol: lastMsg.symbol,
+      text: lastMsg.text,
+    });
+  }
+
+  spinner.text = ` ${message}`;
+  lastMsg = {
+    symbol: `${symbol} `,
+    text: message,
+  };
+
+  spinner.start();
+}
+
+export function stopSpinner(persist: boolean): void {
+  if (lastMsg && persist) {
+    spinner.stopAndPersist({
+      symbol: lastMsg.symbol,
+      text: lastMsg.text,
+    });
+  } else {
+    spinner.stop();
+  }
+
+  lastMsg = undefined;
+}
+
+export function pauseSpinner(): void {
+  spinner.stop();
+}
+
+export function resumeSpinner(): void {
+  spinner.start();
+}
