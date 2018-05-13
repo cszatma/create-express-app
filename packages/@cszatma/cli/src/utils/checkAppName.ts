0f70f96c09b7a55ded0b83340488b331385a4c59
cszatma <christopherszatmary@gmail.com>
2018-05-13 18:10:22 -0400

    Added checkAppName() which verifies a valid project name was given

---
+++ packages/@cszatma/cli/src/utils/checkAppName.ts
@@ -0,0 +1,27 @@
+import chalk from 'chalk';
+import validateProjectName from 'validate-npm-package-name';
+import { logError } from 'node-shared-utils';
+
+export default function checkAppName(appName: string): void {
+  const validationResult = validateProjectName(appName);
+
+  if (!validationResult.validForNewPackages) {
+    logError(
+      `Could not create a project called ${chalk.cyan(
+        `"${appName}"`,
+      )} because of npm naming restrictions:`,
+    );
+
+    printValidationResults(validationResult.errors);
+    printValidationResults(validationResult.warnings);
+    process.exit(1);
+  }
+}
+
+function printValidationResults(results?: string[]): void {
+  if (typeof results !== 'undefined') {
+    results.forEach(error => {
+      console.error(chalk.red(`  *  ${error}`));
+    });
+  }
+}
