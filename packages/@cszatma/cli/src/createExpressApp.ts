7425f8b4c06a3771438c3b5e6d2c6a895a4a9c4c
cszatma <christopherszatmary@gmail.com>
2018-05-17 23:01:01 -0400

    Added remove-preset command to cli

--- packages/@cszatma/cli/src/createExpressApp.ts
+++ packages/@cszatma/cli/src/createExpressApp.ts
@@ -1,6 +1,8 @@
 import { Command } from 'commander';
+import { exitSuccess } from 'node-shared-utils';
 
 import create from './create';
+import { removePreset } from './options';
 
 const packageJson = require('../package.json');
 
@@ -12,6 +14,22 @@ const program = new Command(packageJson.name)
   .option('--use-npm', 'Use npm as the package manager')
   .option('--no-git', 'Does not create a git repo in the project')
   .action(create);
+
+program
+  .command('remove-preset <preset-name>')
+  .description('Removes the given custom preset')
+  .usage('remove-preset <preset-name>')
+  .action((presetName: string) => {
+    const status = removePreset(presetName);
+
+    if (status === 1) {
+      console.log(`${presetName} preset does not exist. Nothing to delete!\n`);
+      process.exit(0);
+      return;
+    } else {
+      exitSuccess(`Removed ${presetName} preset.\n`);
+    }
+  });
 
 program.parse(process.argv);
 
