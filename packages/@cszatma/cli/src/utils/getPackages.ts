f8014801172592b0051f81b46e782c0e35379a22
cszatma <christopherszatmary@gmail.com>
2018-05-17 23:10:16 -0400

    Added babel, typescript and react packages to getPackages()

--- packages/@cszatma/cli/src/utils/getPackages.ts
+++ packages/@cszatma/cli/src/utils/getPackages.ts
@@ -2,20 +2,25 @@ import path from 'path';
 
 const packagesDir = path.join(__dirname, '../../../../');
 
+// Resolve other packages in the repo when running locally
 export const resolveLocal = (packageName: string) =>
   path.resolve(packagesDir, packageName);
 
+// Resolve a the project's dependency rather than the cli's
 export const resolveProjectDep = (packageName: string) =>
   path.resolve(process.cwd(), 'node_modules', packageName);
 
 export default function getPackages() {
   const packages: { [key: string]: string } = {
     expressScripts: '@cszatma/express-scripts',
-    linter: '@cszatma/express-plugin-linter',
+    babel: '@cszatma/express-plugin-babel',
     typescript: '@cszatma/express-plugin-typescript',
+    linter: '@cszatma/express-plugin-linter',
+    react: '@cszatma/express-plugin-react',
     sharedUtils: 'node-shared-utils',
   };
 
+  // CLI is being run locally, replace all the packages with local paths
   if (process.env.CREATE_EXPRESS_APP_TEST) {
     Object.keys(packages).forEach(
       key => (packages[key] = `file:${resolveLocal(packages[key])}`),
