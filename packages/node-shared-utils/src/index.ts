87396d509121fe394296114fe36f8d600223c381
cszatma <christopherszatmary@gmail.com>
2018-05-17 23:24:28 -0400

    Added capitalizeString() function to node-shared-utils

--- packages/node-shared-utils/src/index.ts
+++ packages/node-shared-utils/src/index.ts
@@ -1,5 +1,7 @@
 export * from './commandExists';
 export * from './logger';
 export * from './spinner';
+export * from './stringUtils';
 
 export type Optional<T> = T | undefined;
+export type Nullable<T> = T | null;
